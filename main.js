58ccb6766f5067008a9b9de96b8ba497f66375a5
Josh Viktorov <joshviktorov27@gmail.com>
2019-08-06 16:59:38 -0400

    work on gen info and breakdown cards

--- main.js
+++ main.js
@@ -1,22 +1,75 @@
 $(document).ready(function() { 
 
     $('button.submit').on('click', () => {
+
+        $(".card .info").html('');
+
         let json = $('textarea').val();
         let newJson = json.replace(/'/g, '\"');
         let parsed = JSON.parse(newJson);
         let team = $('input[name="team"]:checked').val();
         let half = (team==="Home" ? "top" : "bottom");
 
-        analyze(parsed, half);
+        let stats = analyze(parsed, half);
+
+        $('.info').append("<li>Number of Defensive Plays: " + stats[0] + "</li>");
+        $('.info').append("<li>Innings Played: " + stats[1] + "</li>");
+        $('.info').append("<li>Runs Allowed: " + stats[3] + "</li>");
+
+        for(let play in stats[2]) {
+            $('ul.plays').append("<li>" + play + " " + stats[2][play] + "</li>");
+        }
 
     });
 
 });
 
 let analyze = (json, half) => {
-    let fieldingPlays = json.plays.filter(play => play.half === half);;
+    let fieldingPlays = json.plays.filter(play => play.half === half);
     let numberOfPlays = fieldingPlays.length;
 
-    $('p.info').html("Number of Plays: " + numberOfPlays);
+    let innings = inningCounter(json);
 
+    let allResults = fieldingPlays.map(play => play.result);
+
+    let sortedResults = playSorter(allResults);
+
+    let RBIs = rbiCounter(fieldingPlays);
+
+    return [numberOfPlays, innings, sortedResults, RBIs];
+}
+
+let playSorter = (arr) => {
+    let counts = [];
+
+    for(let i=0;i<arr.length;i++) {
+        if(counts[arr[i]] === undefined) {
+            counts[arr[i]] = 1;
+        } else {
+            counts[arr[i]] = counts[arr[i]] + 1;
+        }
+    }
+
+    return counts;
+}
+
+let inningCounter = (json) => {
+    let innings = 0;
+    let prevHalf = "top";
+    for(let i=0;i<json.plays.length;i++) {
+        let currHalf = json.plays[i].half;
+        if(currHalf != prevHalf) {
+            innings++;
+        }
+        prevHalf=currHalf;
+    }
+    innings = Math.ceil(innings);
+
+    return innings;
+}
+
+let rbiCounter = (json) => {
+    return json.filter((play) => {
+        return play.result.match(/rbi/gi)
+    }).length;
 }
